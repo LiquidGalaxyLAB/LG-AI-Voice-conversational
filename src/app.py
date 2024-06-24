b81205e2e2f29aa1443aaed852cc0ea7b5ded7bf
ryanmckim <mck649@gmail.com>
2024-06-24 01:27:48 -0700

    Implemented text to speech models, except ChatTTS due to internal server errors.

--- src/app.py
+++ src/app.py
@@ -1,17 +1,23 @@
 import os
+import uuid
 import requests
+from dotenv import load_dotenv
 from fastapi import FastAPI, HTTPException, Request
 from fastapi.responses import FileResponse
 from groq import Groq
-from dotenv import load_dotenv
-from bark import SAMPLE_RATE, generate_audio, preload_models
-from scipy.io.wavfile import write as write_wav
+from bark import SAMPLE_RATE, generate_audio
+from elevenlabs import VoiceSettings
+from elevenlabs.client import ElevenLabs
+from deepgram import DeepgramClient, ClientOptionsFromEnv, SpeakOptions
+from google.cloud import texttospeech
+# import ChatTTS
+# import torch
 from IPython.display import Audio
+from scipy.io.wavfile import write as write_wav
 from tempfile import NamedTemporaryFile
 from model_config import MODEL_CONFIGS
 
 load_dotenv()
-client = Groq(api_key=os.environ.get("GROQ_API_KEY"))
 app = FastAPI()
 
 @app.post("/speech-to-text/")
@@ -61,23 +67,105 @@ async def speech_to_text(request: Request):
 async def text_to_speech_bark(request: Request):
     data = await request.json()
     model = data.get("model")
+    content = data.get("content")
 
     model_config = MODEL_CONFIGS.get(model)
     if not model_config:
         raise HTTPException(status_code=400, detail="Model not found.")
+
+    if model == "google_cloud_tts":
+        try:
+            client = texttospeech.TextToSpeechClient()
+            synthesis_input = texttospeech.SynthesisInput(text=content)
+            voice = texttospeech.VoiceSelectionParams(language_code="en-US", name="en-US-Wavenet-D")
+            audio_config = texttospeech.AudioConfig(audio_encoding=texttospeech.AudioEncoding.LINEAR16)
+            response = client.synthesize_speech(input=synthesis_input, voice=voice, audio_config=audio_config)
+
+            with NamedTemporaryFile(delete=False, suffix=".wav") as temp_file:
+                temp_file.write(response.audio_content)
+                temp_file_path = temp_file.name
+
+            return FileResponse(
+                temp_file_path, media_type="audio/wav", filename="google_cloud.wav"
+            )
+
+        except Exception as e:
+            raise HTTPException(status_code=500, detail=f"An error occurred: {str(e)}")
     
     if model == "bark_tts":
-        content = data.get("content")
-
         try:
             audio_array = generate_audio(content)
             with NamedTemporaryFile(delete=False, suffix=".wav") as temp_file:
                 write_wav(temp_file.name, SAMPLE_RATE, audio_array)
                 temp_file_path = temp_file.name
-            return FileResponse(temp_file_path, media_type="audio/wav", filename="bark_generation.wav")
+            return FileResponse(temp_file_path, media_type="audio/wav", filename="bark.wav")
 
         except Exception as e:
             raise HTTPException(status_code=500, detail=f"An error occurred: {str(e)}")
+
+
+    if model == "elevenlabs_tts":
+        ELEVENLABS_API_KEY = os.getenv("ELEVENLABS_API_KEY")
+
+        if not ELEVENLABS_API_KEY:
+            raise ValueError("ELEVENLABS_API_KEY is missing.")
+        
+        try:
+            client = ElevenLabs(api_key=ELEVENLABS_API_KEY)
+                    
+            response = client.text_to_speech.convert(
+            voice_id="pNInz6obpgDQGcFmaJgB",
+            optimize_streaming_latency="0",
+            output_format="mp3_22050_32",
+            text=content,
+            model_id="eleven_turbo_v2",
+            voice_settings=VoiceSettings(
+                stability=0.0,
+                similarity_boost=1.0,
+                style=0.0,
+                use_speaker_boost=True,
+                )
+            )
+            save_file_path = f"{uuid.uuid4()}.mp3"
+
+            with open(save_file_path, "wb") as f:
+                for chunk in response:
+                    if chunk:
+                        f.write(chunk)
+            return FileResponse(save_file_path, media_type="audio/mpeg", filename="elevenlabs.mp3")
+
+        except Exception as e:
+            raise HTTPException(status_code=500, detail=f"An error occurred: {str(e)}")
+
+
+    if model == "deepgram_tts":
+        DEEPGRAM_API_KEY = os.getenv("DEEPGRAM_API_KEY")
+
+        if not DEEPGRAM_API_KEY:
+            raise ValueError("DEEPGRAM_API_KEY is missing.")
+        
+        try:
+            deepgram = DeepgramClient(api_key=DEEPGRAM_API_KEY, config=ClientOptionsFromEnv())
+            options = SpeakOptions(model="aura-asteria-en")
+            response = await deepgram.asyncspeak.v("1").save("deepgram.mp3", {"text": content}, options)
+            return FileResponse("deepgram.mp3", media_type="audio/mpeg", filename="deepgram.mp3")
+
+        except Exception as e:
+            raise HTTPException(status_code=500, detail=f"An error occurred: {str(e)}")
+    
+    # if model == "chatt_tts":
+    #     try:
+    #         chat = ChatTTS.Chat()
+    #         chat.load_models()
+    #         wavs = chat.infer([content], use_decoder=True)
+
+    #         with NamedTemporaryFile(delete=False, suffix=".wav") as temp_file:
+    #             write_wav(temp_file.name, 24000, wavs[0])
+    #             temp_file_path = temp_file.name
+    #         return FileResponse(temp_file_path, media_type="audio/wav", filename="chatt_tts.wav")
+
+    #     except Exception as e:
+    #         raise HTTPException(status_code=500, detail=f"An error occurred: {str(e)}")
 
 @app.post("/groq/")
 async def create_chat_completion(request: Request):
@@ -85,6 +173,7 @@ async def create_chat_completion(request: Request):
     model = data.get("model")
     content = data.get("content")
     model_config = MODEL_CONFIGS.get("groq")
+    client = Groq(api_key=os.environ.get("GROQ_API_KEY"))
     models = ["llama3-8b-8192", "llama3-70b-8192", "mixtral-8x7b-32768", "gemma-7b-it"]
 
     if not model_config:
